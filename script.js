0f86eb241b4d01663dc7b73cf280a6d966528cf7
Ollie Baker <oliverjohnbaker@gmail.com>
2020-05-26 09:15:29 +0100

    WIP refactor to use timer as module.

--- script.js
+++ script.js
@@ -4,86 +4,49 @@ const display = {
   minutes: document.querySelector('#display >#minutes'),
   seconds: document.querySelector('#display >#seconds'),
   millis: document.querySelector('#display >#millis'),
-  eventList: document.querySelector('#event-list')
+  eventList: document.querySelector('#event-list'),
 };
 
-const subscriptions = [];
-
-const subscribe = (delay, cb) => {
-  subscriptions.push({
-    start: Date.now(),
-    delay,
-    cb
-  });
-
-  if (subscriptions.length > 1) {
-    subscriptions.sort((a, b) => {
-      return a.delay > b.delay;
-    })
-  }
-}
-
-let time = 0;
 const pad = (input) => {
   return input.toString().length < 2 ? `0${input}` : input;
-}
-let log = 0;
-const animate = (start, delta) => {
-  //update time
-  // const now = Date.now();
-  const progress = (delta - start);
-  
-
-  if(++log < 5) {
-    console.log(
-      'start',start,
-      'delta', delta,
-      // 'now', now,
-      'progress', progress,
-      'time', time,
-    )
-  }
-  
-
-  //display
-  const minutes = Math.floor(time / 60000);
-  display.minutes.innerHTML = minutes;
-  const seconds = Math.floor(((time % 60000) / 1000)).toFixed(0);
-  display.seconds.innerHTML = pad(seconds);
-  const millis = Math.floor(((time % 1000) / 10)).toFixed(0);
-  display.millis.innerHTML = pad(millis);
-
-   for (let i = subscriptions.length - 1; i >= 0; i--) {
-    const {start, delay, cb} = subscriptions[i];
-    const expired = Date.now() - start >= delay;
-    if (expired) {
-      const newLi = document.createElement('li');
-      newLi.innerHTML = `Complete at ${time / SECOND}s, after ${delay / SECOND}s.`
-      display.eventList.appendChild(newLi);
-      if (cb) {
-        console.log(delay);
-        cb();
-      }
-      subscriptions.splice(i, 1);
-    }
-  }
-
-  //update time
-  time += progress;
-
-  //repeat
-  requestAnimationFrame((nd) => animate(delta, nd));
 };
 
-subscribe(SECOND * 3, () => { 
-  subscribe(SECOND * 3, () => {console.log('DONE 2!') })
-  console.log('DONE!') 
-});
+console.log(timer);
 
-subscribe(SECOND * 30, () => { console.log('DONE 30') }); 
+function onUpdate(time) {
+  ['minutes', 'seconds', 'millis'].forEach((key) => {
+    const el = display[key];
+    const value = pad(time[key]);
+    el.innerHTML = value;
+  });
+}
 
 function start() {
-  requestAnimationFrame((delta) => animate(delta, delta));
+  timer.start(onUpdate);
+
+  timer.subscribe(SECOND * 3, () => {
+    timer.subscribe(SECOND * 3, () => {
+      console.log('DONE 2!');
+    });
+    timer.pause();
+    console.log('DONE!');
+  });
+
+  timer.subscribe(SECOND * 30, () => {
+    console.log('DONE 30');
+    timer.stop();
+  });
+
+  setTimeout(() => {
+    // console.log('RIVAL 3');
+  }, SECOND * 3);
+  setTimeout(() => {
+    // console.log('RIVAL 6');
+    timer.resume();
+  }, SECOND * 6);
+  setTimeout(() => {
+    // console.log('RIVAL 30');
+  }, SECOND * 30);
 }
 
 start();
